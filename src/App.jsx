ee56d6d17f1f46108cb3cc6ccbee5bf5403718fd
Abdurhman-kamel <abood.kamel17@gmail.com>
2026-05-15 10:21:49 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import  '@fortawesome/fontawesome-free/css/all.min.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button className="btn btn-warning">loading...</button>
+      <div>
+        <i className="fa-solid fa-trash"></i>
+      </div>
+    </>
+  );
+}
+
+export default App;
